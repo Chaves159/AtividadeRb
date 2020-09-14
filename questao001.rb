63c00ad61e9c20312384e25aa8104b152726b840
João Victor Chaves <joao.victor.chaves159@gmail.com>
2020-09-13 21:06:59 -0300

    Concluido update

--- questao001.rb
+++ questao001.rb
@@ -2,4 +2,4 @@ print "Digite seu nome: "
 nome = gets.chomp
 tamanho = nome.length
 puts "Seu nome é:#{nome}"
-puts "Comprimento do nome: #{tamanho} caracteres
+puts "Comprimento do nome: #{tamanho} caracteres"
