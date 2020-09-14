312c8383289fdc53ea50ea6b96b3e4a923d3e961
João Victor Chaves <joao.victor.chaves159@gmail.com>
2020-09-13 21:30:39 -0300

    Tarefa 009 update

--- questao009.rb
+++ questao009.rb
@@ -2,48 +2,28 @@ num = []
 par = []
 impar = []
 i = 0
- while i < 20
+ while i < 6
  	print "Digite um valor: "
-
  	num[i] = gets.chomp.to_i
-
- 	i = i + 1
-  end
-
-i = 0
-
-while i < num.size
 	if num[i] %2 == 0
-		
 		par[i] = num[i]
-		
 		else
 			impar[i] = num[i]
 	end
-
-	i = i + 1
-
-end
-
+ 	i = i + 1
+  end
 print "Vetor completo: {"
-
 num.each do |i|
 	print "  #{num[i-1]}"
 end
-
 print "}"
-
-
 print "\nVetor Impar: {"
 num.each do |i|
 	print " #{impar[i-1]}"
-
 end
 print "}"
-
 print "\nVetor Par: {"
 num.each do |i|
 	print " #{par[i-1]}"
 end
-
 print "}"
